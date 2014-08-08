82f118029b08db064dafab883bfaccbf610080a3
unknown <hoa@NTX-ARTHUR.Nintex.com>
2014-08-08 14:05:31 +1000

    missing backend file

---
+++ src/routes/newbackend.js
@@ -0,0 +1,12 @@
+﻿var app = require('../app');
+
+exports.addNewBackEnd = function (req, res) {
+    if (req.method == 'GET') {
+        app.backends.push(req.query.p);
+        res.status(201);
+    } else {
+        res.status(204);
+    }
+    
+    res.send();
+};
